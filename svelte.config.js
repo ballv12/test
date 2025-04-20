861a6c2508ac4581dd6d95ca4bfad62f2c40506c
ball <ballcruzv@gmail.com>
2025-04-19 23:36:28 -0700

    fix: update static adapter configuration and add .nojekyll

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
     adapter: adapter({
-      fallback: '404.html'
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
     }),
     paths: {
       base: process.env.NODE_ENV === 'production' ? '/bell-main' : ''
